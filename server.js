a6e393a1df2ae4fdc8f50713e375d901a765d3ca
yunglordsimens <79109617+yunglordsimens@users.noreply.github.com>
2024-06-14 17:10:56 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const OAuth = require('oauth').OAuth;
+const app = express();
+const port = 3000;
+
+const tumblrConsumerKey = '4t7uUNbPNa5nZnZxaICHOe9O1WXfeL4J2lN0UosLNv0PLhEsXZ';
+const tumblrConsumerSecret = 'show';
+const callbackUrl = 'http://localhost:3000/callback';
+
+const oa = new OAuth(
+  'https://www.tumblr.com/oauth/request_token',
+  'https://www.tumblr.com/oauth/access_token',
+  tumblrConsumerKey,
+  tumblrConsumerSecret,
+  '1.0A',
+  callbackUrl,
+  'HMAC-SHA1'
+);
+
+app.get('/auth', (req, res) => {
+  oa.getOAuthRequestToken((error, oauthToken, oauthTokenSecret, results) => {
+    if (error) {
+      res.send('Error getting OAuth request token: ' + error);
+    } else {
+      req.session.oauthToken = oauthToken;
+      req.session.oauthTokenSecret = oauthTokenSecret;
+      res.redirect('https://www.tumblr.com/oauth/authorize?oauth_token=' + oauthToken);
+    }
+  });
+});
+
+app.get('/callback', (req, res) => {
+  const oauthToken = req.session.oauthToken;
+  const oauthTokenSecret = req.session.oauthTokenSecret;
+  const oauthVerifier = req.query.oauth_verifier;
+
+  oa.getOAuthAccessToken(oauthToken, oauthTokenSecret, oauthVerifier, (error, oauthAccessToken, oauthAccessTokenSecret, results) => {
+    if (error) {
+      res.send('Error getting OAuth access token: ' + error);
+    } else {
+      req.session.oauthAccessToken = oauthAccessToken;
+      req.session.oauthAccessTokenSecret = oauthAccessTokenSecret;
+      res.redirect('/posts');
+    }
+  });
+});
+
+app.get('/posts', (req, res) => {
+  const oauthAccessToken = req.session.oauthAccessToken;
+  const oauthAccessTokenSecret = req.session.oauthAccessTokenSecret;
+
+  oa.get('https://api.tumblr.com/v2/blog/anime--irl.tumblr.com/posts?api_key=' + tumblrConsumerKey, oauthAccessToken, oauthAccessTokenSecret, (error, data, response) => {
+    if (error) {
+      res.send('Error getting Tumblr posts: ' + error);
+    } else {
+      const posts = JSON.parse(data).response.posts;
+      res.json(posts);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
